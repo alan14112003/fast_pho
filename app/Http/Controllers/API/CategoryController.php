9e48751baa31c6d1a543a27f4d312f738a2873ac
alan14112003 <sonnn.21it@gmil.com>
2023-06-07 13:57:15 +0700

    Add crud Category

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\Category\UpdateRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -25,7 +26,7 @@ class CategoryController extends Controller
         return $categories;
     }
 
-    public function all()
+    public function all(): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         $categories = Category::query()->whereNull('parent_id')->get();
 
@@ -54,8 +55,86 @@ class CategoryController extends Controller
             return response([
                 'status' => false,
                 'body' => null,
-                'message' => "Có lỗi {$e->getMessage()}"
+                'message' => "Có lỗi! {$e->getMessage()}"
             ]);
         }
     }
+
+    public function update($id, UpdateRequest $request): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        try {
+            $category = Category::query()->find($id);
+
+            if (is_null($category)) {
+                return response([
+                    'status' => false,
+                    'body' => null,
+                    'message' =>'Id không tồn tại',
+                ]);
+            }
+
+            if($request->get('name') === $category->name) {
+                return response([
+                    'status' => true,
+                    'body' => $category,
+                    'message' => "Thành công"
+                ]);
+            }
+
+            $category->slug = null;
+            $category->update($request->validated());
+
+            return response([
+                'status' => true,
+                'body' => $category,
+                'message' => "Thành công"
+            ]);
+        } catch (\Exception $e) {
+            return response([
+                'status' => false,
+                'body' => null,
+                'message' => "Có lỗi! {$e->getMessage()}"
+            ]);
+        }
+    }
+
+    public function delete($id): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        try {
+            $category = Category::query()->find($id);
+
+            if (is_null($category)) {
+                return response([
+                    'status' => false,
+                    'body' => null,
+                    'message' =>'Id không tồn tại',
+                ]);
+            }
+
+            $this->deleteChildren($id);
+            $category->delete();
+            return response([
+                'status' => true,
+                'body' => null,
+                'message' =>'Thành công',
+            ]);
+        } catch (\Throwable $e) {
+            return response([
+                'status' => false,
+                'body' => null,
+                'message' => "Có lỗi! {$e->getMessage()}"
+            ]);
+        }
+    }
+
+    public function deleteChildren($parentId)
+    {
+        $categoryIds = Category::query()->where('parent_id', $parentId)->pluck('id')->toArray();
+        Category::query()->whereIn('id', $categoryIds)->delete();
+
+        foreach ($categoryIds as $categoryId) {
+            $this->deleteChildren($categoryId);
+        }
+    }
+
 }
