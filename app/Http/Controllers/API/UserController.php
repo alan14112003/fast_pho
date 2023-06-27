b474eaa4a3dfee8372b248b4a1e69af0055770e1
AlanSNguyen <alannguyen1411@gmail.com>
2023-06-27 08:41:40 +0700

    Create Update Address For User

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\API;
 use App\Enums\UserRoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ResponseTrait;
+use App\Http\Requests\User\UpdateAddressRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -14,7 +15,32 @@ class UserController extends Controller
 
     public function countUser(): \Illuminate\Http\JsonResponse
     {
-        $userCount = User::query()->where('role', '=',UserRoleEnum::USER)->count();
+        $userCount = User::query()->where('role', '=', UserRoleEnum::USER)->count();
         return $this->responseTrait('Thành công', true, $userCount);
+    }
+
+    public function updateAddress(UpdateAddressRequest $request)
+    {
+        try {
+            if (Auth::check()) {
+                if (
+                    Auth::user()->province != $request->get('province') ||
+                    Auth::user()->district != $request->get('district') ||
+                    Auth::user()->ward != $request->get('ward') ||
+                    Auth::user()->address != $request->get('address')
+                ) {
+                    User::query()->find(Auth::id())->update([
+                        'province' => $request->get('province'),
+                        'district' => $request->get('district'),
+                        'ward' => $request->get('ward'),
+                        'address' => $request->get('address'),
+                    ]);
+                }
+            } else {
+                return $this->responseTrait('Đăng nhập tài khoản trước khi lưu địa chỉ!');
+            }
+        } catch (\Throwable $e) {
+            return $this->responseTrait('Có lỗi! ' . $e->getMessage());
+        }
     }
 }
